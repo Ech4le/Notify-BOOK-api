dc30a6b4bd3b402eaf03398629c5f45b80b8d273
nsl <k.nawara98@gmail.com>
2021-01-22 20:18:04 +0100

    Chapter 7 final

--- src/schema.js
+++ src/schema.js
@@ -2,10 +2,14 @@ const { ApolloServer, gql } = require('apollo-server-express');
 
 //Utworzenie schematu za pomocą jezyka schematu GraphQL
 module.exports = gql `
+    scalar DateTime
+
     type Note {
         id: ID!
         content: String!
         author: String!
+        createdAt: DateTime!
+        updatedAt: DateTime!
     }
 
     type Query {
@@ -16,6 +20,8 @@ module.exports = gql `
 
     type Mutation {
         newNote(content: String!): Note!
+        updateNote(id: ID!, content: String!): Note!
+        deleteNote(id: ID!): Boolean!
     }
 `;
 
